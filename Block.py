1dfaf6ecf58e03aa009f07fc2911f672555a3c24
zarif007 <zarifhuq786@gmail.com>
2021-01-10 17:25:39 +0600

    Added addblock, genesisblock function and sha-256

---
+++ Block.py
@@ -0,0 +1,45 @@
+import time
+
+from crypto_hash import crypto_hash
+
+
+class Block:
+    """Unit of storage in BlockChain that stores transictions"""
+    def __init__(self, timestamp, last_hash, hash, data):
+        self.timestamp = timestamp
+        self.last_hash = last_hash
+        self.data = data
+        self.hash = hash
+    
+    def __repr__(self):
+        """Block represntation"""
+        return (
+            'Block('
+            f'TimeStamp -> {self.timestamp}, '
+            f'Last_Hash -> {self.last_hash}, '
+            f'Hash -> {self.hash}, '
+            f'data -> {self.data} )'
+        )
+    
+    @staticmethod
+    def mine_block(last_block, data):
+        """Mine a block based on the last given block and data"""
+        timestamp = time.time_ns()
+        last_hash = last_block.hash
+        hash = crypto_hash(timestamp, last_hash, data)
+
+        return Block(timestamp, last_hash, hash, data)
+
+    @staticmethod
+    def genesis():
+        """Generate the genesis block"""
+        return Block(1, 'genesis_last_hash', 'genesis_hash', [])
+
+
+def main():
+    genesis_block = Block.genesis()
+    block = Block.mine_block(genesis_block, 'haha')
+    print(block)
+
+if __name__ == '__main__':
+    main()
---
+++ crypto_hash.py
@@ -0,0 +1,14 @@
+import hashlib
+import json
+
+def crypto_hash(*args):
+    """Return a sha-256 hash of given arguments"""
+    stringified_args = sorted(map(lambda data: json.dumps(data), args))
+    joined_data = ''.join(stringified_args)
+    return hashlib.sha256(joined_data.encode('utf-8')).hexdigest()
+
+def main():
+    print(f'cryptohash -> {crypto_hash(66, 22, "zarif")}')
+
+if __name__ == '__main__':
+    main()
